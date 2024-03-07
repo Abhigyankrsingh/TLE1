5d58dc0ba5c72b0447c61cde00588163dce8ff7d
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-07 19:03:37 +0530

    Code_Updated

---
+++ q3.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+#define int long long
+
+#define endl '\n'
+
+
+
+
+signed main(){
+
+ ios::sync_with_stdio(false); cin.tie(NULL);
+
+  int a, b;
+
+  cin >> a >> b;
+
+  int rem1 = a % 10 ;
+  int rem2 = b %10;
+
+  cout << rem1 + rem2 << endl;
+
+
+}
