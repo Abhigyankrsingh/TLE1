22361457f4d0b5fb5aae41c0ed53cb1eb5e39f3c
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-07 18:12:14 +0530

    J. Multiples

---
+++ q2.cpp
@@ -0,0 +1,28 @@
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+
+#define int long long int
+
+
+
+
+signed main(){
+
+   ios::sync_with_stdio(false); cin.tie(NULL);
+
+   int a , b;
+
+   cin >> a >> b;
+
+   if(b % a == 0){
+    cout << "Multiples";
+   } else {
+     cout << "No Multiples";
+   }
+
+}
