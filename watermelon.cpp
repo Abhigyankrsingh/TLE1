6409bb188f2052c728e37c57b8bf09618818de0f
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-08 13:34:00 +0530

    https://codeforces.com/problemset/problem/4/A

---
+++ watermelon.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+#define endl '\n'
+
+signed main(){
+
+    int a;
+    cin >> a;
+
+  if ((a % 2 ==1) || (a == 2)){
+    cout << "NO"<<endl;
+  } else {
+    cout << "YES"<<endl;
+  }
+}
