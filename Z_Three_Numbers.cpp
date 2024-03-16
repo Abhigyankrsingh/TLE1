c516ae6c1fe35b452aa67a2dfd6b371a31a76cca
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-17 02:24:07 +0530

    Code_Updated

--- Z_Three_Numbers.cpp
+++ Z_Three_Numbers.cpp
@@ -301,6 +301,26 @@ int32_t main()
 
 //    cout << cn << endl;
 
+int k,s;
+
+cin >> k >>s;
+
+int cn =0;
+
+for(int x =0; x <=k;x++){
+    for(int y =0; y<=k;y++){
+     int z = s- x - y;
+
+     if( z >=0 && z <=k){
+        cn ++;
+     }   
+    }
+}
+
+cout << cn << endl;
+
+return 0;
+
 }
 
     
