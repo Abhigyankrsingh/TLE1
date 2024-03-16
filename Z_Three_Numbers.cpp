14349ff0d81a169b967117ef884d1000539975cd
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-03-16 17:26:32 +0530

    Code_updated

--- Z_Three_Numbers.cpp
+++ Z_Three_Numbers.cpp
@@ -283,14 +283,24 @@ int32_t main()
  
  ios_base::sync_with_stdio(false);
  cin.tie(NULL);
+  
+//   int k, s;
+ 
+//   cin >> k>>s;
+//      int cn = 0;
+//    for( int  x = 0; x <=k;x++){
+//     for(int y =0; y<=k;y++){
+//         for(int z = 0; z<=k; z++){
 
-    int T = 1;
-    cin >> T;
-    while (T--)
-    {
-        solve();
-    }
-    return 0;
+//             if( x + y+ z == s){
+//                 cn++;
+//             }
+//         }
+//     }
+//    }
+
+//    cout << cn << endl;
+
 }
 
     
